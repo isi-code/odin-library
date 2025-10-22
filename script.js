11e09395c10a1a0972f0c090981f3ff6611e38e7
isi-code <irvinjose.silva@gmail.com>
2025-10-22 16:28:16 -0700

    Determine properties a book object will have

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(id) {
+    // the constructor..
+    this.id = id;
+    this.book = {};
+}
+
+function addBookToLibrary(title,genre,description,author,pictureUrl,releaseDate) {
+  // take params, create a book then a store it in the array
+}
+
