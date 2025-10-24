90d32f18add417996627e60e51d1b06510c7dd93
isi-code <irvinjose.silva@gmail.com>
2025-10-24 16:29:17 -0700

    Add `read` property to Book object constructor

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 
-function Book(title,genre,description,author,pictureUrl,releaseDate,pages) {
+function Book(title,genre,description,author,pictureUrl,releaseDate,pages,read) {
   // the constructor..
   if (!new.target){
     throw Error("You must use the 'new' operator to call the constructor");
@@ -13,6 +13,7 @@ function Book(title,genre,description,author,pictureUrl,releaseDate,pages) {
   this.description = description;
   this.pictureUrl = pictureUrl;
   this.releaseDate = releaseDate;
+  this.read = read;
 
   this.makeBook = function() {
     const book = {};
@@ -25,14 +26,15 @@ function Book(title,genre,description,author,pictureUrl,releaseDate,pages) {
     book.pictureUrl = this.pictureUrl;
     book.releaseDate = this.releaseDate;
     book.pages = this.pages;
+    book.read = this.read;
 
     return book
   };
 };
 
-function addBookToLibrary(title,genre,description,author,pictureUrl,releaseDate,pages) {
+function addBookToLibrary(title,genre,description,author,pictureUrl,releaseDate,pages,read) {
   // take params, create a book then a store it in the array
-  const book = new Book(title,genre,description,author,pictureUrl,releaseDate,pages);
+  const book = new Book(title,genre,description,author,pictureUrl,releaseDate,pages,read);
   myLibrary.push(book.makeBook())
 }
 
@@ -43,7 +45,7 @@ const example = addBookToLibrary(
   "https://upload.wikimedia.org/wikipedia/en/0/05/Littleprince.JPG",
   "Antoine de Saint-Exupéry",
   "April 1943",
-  "96 pages"
+  "96 pages",
+  true
 );
 
-console.log(myLibrary)
